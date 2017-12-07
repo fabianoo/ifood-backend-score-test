f473db464b26eacece68a01a97ae636182297d83
Fabiano Oliveira <email@fabiano.pro>
2017-12-07 00:58:36 -0200

    Removing code

--- getup-with-docker.sh
+++ getup-with-docker.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 sh mvnw clean install
 
 docker-compose rm -f
 docker rmi -f backendscoretest_app
 docker-compose up -d --force-recreate
-
-#docker rm -f score-app
-
-#docker build score-module-application -t score-app
-#docker run -t score-app
